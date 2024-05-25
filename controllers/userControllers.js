db8510fcdf2fe882d44782c9d1f72239c15d6697
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-05-25 19:42:01 +0100

    check

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,5 +1,28 @@
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
+import HttpError from '../helpers/HttpError.js';
+
 import User from '../models/users.js';
 
 export const uploadAvatar = async (req, res, next) => {
-	res.send('Upload avatar');
+	try {
+		await fs.rename(
+			req.file.path,
+			path.resolve('public/avatars', req.file.filename)
+		);
+
+		const newUser = await User.findByIdAndUpdate(
+			req.user.id,
+			{ avatar: req.file.filename },
+			{ new: true }
+		);
+
+		if (newUser === null) {
+			throw HttpError(404, 'User not found');
+		}
+
+		res.send(newUser);
+	} catch (error) {
+		next(error);
+	}
 };
