cd5e7a900fedd4342d1d0c99af525c5259753182
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-05-30 13:00:48 +0100

    fillup

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import './db.js';
+import routes from './routes/index.js';
+import path from 'node:path';
 
-import contactsRouter from './routes/contactsRouter.js';
+const PORT = 3000;
 
 const app = express();
 
@@ -10,17 +13,23 @@ app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/contacts', contactsRouter);
+app.use('/public', express.static(path.resolve('public')));
 
-app.use((_, res) => {
+app.use('/api', routes);
+
+// Handle 404 Error
+app.use((req, res, next) => {
+	console.log(req.url);
+	console.log(req.method);
 	res.status(404).json({ message: 'Route not found' });
 });
 
+// Handle 500 Error
 app.use((err, req, res, next) => {
 	const { status = 500, message = 'Server error' } = err;
 	res.status(status).json({ message });
 });
 
-app.listen(3000, () => {
-	console.log('Server is running. Use our API on port: 3000');
+app.listen(PORT, () => {
+	console.log(`Server is running. Use our API on port: ${PORT}`);
 });
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import authRoutes from './authRouter.js';
+import contactsRoutes from './contactsRouter.js';
+import userRoutes from './usersRouter.js';
+
+import authMiddleware from '../helpers/auth.js';
+
+const router = express.Router();
+
+router.use('/auth', authRoutes);
+router.use('/contacts', authMiddleware, contactsRoutes);
+router.use('/users', authMiddleware, userRoutes);
+
+export default router;
